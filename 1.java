cb2996ca06291712133c939bf1dcdfb9c0a3a9f8
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2025-02-10 16:45:51 +0530

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,8 @@
+class firstClass{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("I'm Ayush Shivam\n");
+        System.out.print("Learning Java\nLet's crack it.");
+    }
+}
