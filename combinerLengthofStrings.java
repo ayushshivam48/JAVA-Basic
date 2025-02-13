15f62c18fd432df81c743573ebf7db75831874d1
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2025-02-13 19:22:52 +0530

    Add files via upload

---
+++ combinerLengthofStrings.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class combinerLengthofStrings {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        String arr[] = new String[size];
+
+        int totLength = 0;
+
+        for(int i=0;i<size;i++){
+            arr[i] = sc.next();
+            totLength += arr[i].length();
+        }
+        System.out.println(totLength);
+        sc.close();
+    }
+}
