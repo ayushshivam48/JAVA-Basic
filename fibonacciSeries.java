f7cdc8fce2a630e8b4d9957619e09ad68fd78380
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2025-02-14 18:15:37 +0530

    Add files via upload

---
+++ fibonacciSeries.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class fibonacciSeries {
+    public static int fibo(int n){
+        if(n<=1){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i=0;i<n;i++) {
+            System.out.print(fibo(i)+" ");
+        }
+        sc.close();
+    }
+}
