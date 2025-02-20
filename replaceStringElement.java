e3c7d76e79fbf21ddc3670fc0e653531a8e8baec
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2025-02-20 07:57:28 +0530

    Add files via upload

---
+++ replaceStringElement.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class replaceStringElement {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String result = "";
+
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == 'e'){
+                result += 'i';
+            }else{
+                result += str.charAt(i);
+            }
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
